73c20b2f2c71165c416f47d5d5be0e1fd7ef5f9c
Nora Vogt <noraevogt@gmail.com>
2018-10-21 14:40:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten('Ralph') {
-  kittens.push('Ralph')
+  kittens.push('Ralph');
 }
